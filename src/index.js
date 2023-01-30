33d28c09f560842c1ed5da1c017089390bbaa49d
Oksana <moonka.moonka@gmail.com>
2023-01-30 14:59:17 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import "./css/style.css"; //importing css
+
+const modal = document.querySelector(".modal");
+
+const modalPromise = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("block");
+    }, 10000);
+  });
+};
+
+modalPromise().then((result) => (modal.style.display = result));
+
+// const resolvingModalPromise = async () => {
+//   let result = await modalPromise();
+//   modal.style.display = result;
+// };
+// resolvingModalPromise();
+
+const close = document.querySelector(".close");
+
+close.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
